102a19ba797627b1cc7f0b3c170ed0056ff596ef
daemon <0x8BADC0DE@gmail.com>
2016-10-05 01:02:17 -0400

    Initial commit

---
+++ src/main/net/rocketeer/sevens/SevensPlugin.java
@@ -0,0 +1,15 @@
+package net.rocketeer.sevens;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SevensPlugin extends JavaPlugin {
+  @Override
+  public void onEnable() {
+
+  }
+
+  @Override
+  public void onDisable() {
+
+  }
+}
